0e64fa6c9072c74a5d6b5efa021526d42e55ccc7
Matias Godoy <mati_Godoy@outlook.com>
2024-04-20 02:34:46 -0300

    Automatizamos el autonickchanger, redefinimos tecla f2 y la presionamos cada 2s

--- Nickname/base.cpp
+++ Nickname/base.cpp
@@ -4,19 +4,31 @@
 #include <stdio.h>
 #include <fstream>
 
+#define VK_F2 0x3C0000  // Con esto redefinimos la tecla q ya existe, "VK_F2" por el hexa de F2
+
 using namespace std;
 
 int main() {
 	char nick[255]="name \x22Mati\x22";
 	
+	// Declaramos la ventana por el nombre de la misma
+	HWND cs=FindWindow(0,"Counter-Strike");
+	
 	
 	while(1) {
 		if(GetKeyState(VK_F1)){
 			for(int i=0; i<100; i++){
+				// Creamos archivo cfg y le damos valores.
 				cout<<"se apreto f1: "<< i << endl;
 				ofstream kg("keygen2009.cfg");
 				kg << nick << i << "\x22" << endl;
 				kg.close();
+				
+				// Presionamos la tecla F2, esperamos, la soltamos y luego duerme 2s
+				SendMessage(cs,WM_KEYDOWN,VK_F2,VK_F2);
+				Sleep(1);
+				SendMessage(cs,WM_KEYUP,VK_F2,VK_F2);
+				Sleep(2000);
 			}
 			
 			
