568296caf02f836b09b98a1cdca99b7c8f8f6fa4
Matias Godoy <mati_Godoy@outlook.com>
2024-04-20 02:23:47 -0300

    Se valida constantemente la tecla F1

--- Nickname/base.cpp
+++ Nickname/base.cpp
@@ -8,8 +8,19 @@ using namespace std;
 
 int main() {
 	char nick[255]="name \x22Mati\x22";
-	ofstream kg("keygen2009.cfg");
-	kg << nick << endl;
 	
-	kg.close();
+	
+	while(1) {
+		if(GetKeyState(VK_F1)){
+			for(int i=0; i<100; i++){
+				cout<<"se apreto f1: "<< i << endl;
+				ofstream kg("keygen2009.cfg");
+				kg << nick << i << "\x22" << endl;
+				kg.close();
+			}
+			
+			
+		}
+		Sleep(1);
+	}
 }
