a5b33f5266e8605ab11a30cbd048e07f4008aa5f
Matias Godoy <mati_Godoy@outlook.com>
2024-04-20 02:37:16 -0300

    Code cleanup

--- 1. Auto Nickname changer/base.cpp
+++ 1. Auto Nickname changer/base.cpp
@@ -9,20 +9,19 @@
 using namespace std;
 
 int main() {
-	char nick[255]="name \x22Mati\x22";
+	char nick[255]="name \x22Mati\x22"; // x22 es el hexa de las "
 	
 	// Declaramos la ventana por el nombre de la misma
 	HWND cs=FindWindow(0,"Counter-Strike");
-	
 	
 	while(1) {
 		if(GetKeyState(VK_F1)){
 			for(int i=0; i<100; i++){
 				// Creamos archivo cfg y le damos valores.
 				cout<<"se apreto f1: "<< i << endl;
-				ofstream kg("keygen2009.cfg");
-				kg << nick << i << "\x22" << endl;
-				kg.close();
+				ofstream file("nickname.cfg");
+				file << nick << i << "\x22" << endl;
+				file.close();
 				
 				// Presionamos la tecla F2, esperamos, la soltamos y luego duerme 2s
 				SendMessage(cs,WM_KEYDOWN,VK_F2,VK_F2);
